2d730e8a555021bf95d106a9e6aef0d70e159a5d
yuki <38411009+yukijina@users.noreply.github.com>
2025-03-18 07:36:20 -0700

    crate a function for onkey movements

--- snake.py
+++ snake.py
@@ -2,12 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
   """create snakes"""
   def __init__(self):
     self.snakes = []
     self.create_snake()  
+    self.head = self.snakes[0]
     
   def create_snake(self):
     for position in STARTING_POSITION:
@@ -27,7 +32,30 @@ class Snake:
       new_x = self.snakes[snake_num -1].xcor()
       new_y = self.snakes[snake_num -1].ycor()
       self.snakes[snake_num].goto(new_x, new_y)
-  
+      
     #after all snakes are in the same cell, move to the first turtle  
-    self.snakes[0].forward(MOVE_DISTANCE)
+    self.head.forward(MOVE_DISTANCE)
+  
+  #snakes moves one direction only  
+  def move_up(self):
+    """Change snake's direction to up"""
+    if self.head.heading() != DOWN:
+      self.head.setheading(UP)
+  
+  def move_down(self):
+    """Change snake's direction to down"""
+    if self.head.heading() != UP:
+      self.head.setheading(DOWN)
+  
+  def move_left(self):
+    """Change snake's direction to left"""
+    if self.head.heading() != RIGHT:
+      self.head.setheading(LEFT)
+    
+  def move_right(self):
+    """Change snake's directio to right"""
+    if self.head.heading() != LEFT:
+      self.head.setheading(RIGHT)
+ 
+    
     
