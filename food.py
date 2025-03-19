ba2d75677c445de240e46d7294e51dd9715edc7d
yuki <38411009+yukijina@users.noreply.github.com>
2025-03-19 07:50:28 -0700

    create a food class. inherit from turtle

---
+++ food.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+  def __init__(self):
+    super().__init__()
+    self.shape("circle")
+    self.penup()
+    self.shapesize(0.5,0.5)
+    self.color("pink")
+    #self.speed("fastest")
+    random_x = random.randint(-280,280)
+    random_y = random.randint(-280,280)
+  
+    
+
